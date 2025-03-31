8bb92de6765757ca8f6f57f710f37f1822243487
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-03-31 16:30:45 -0700

    Create 2 dropdown menus using click and hover buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
 
-console.log("testing");
+// Click button
+let btn = document.querySelector("#btn1");
+let dropdown = document.querySelector(".dropdown");
+btn.addEventListener("click", () => {
+  dropdown.style.visibility === "visible"
+    ? (dropdown.style.visibility = "hidden")
+    : (dropdown.style.visibility = "visible");
+});
+
+// Hover button
+let btn2 = document.querySelector("#btn2");
+let container2 = document.querySelector("#container2");
+let dropdown2 = document.querySelector("#dropdown2");
+
+btn2.addEventListener("mouseover", () => {
+  dropdown2.style.visibility = "visible";
+});
+container2.addEventListener("mouseleave", () => {
+  dropdown2.style.visibility = "hidden";
+});
